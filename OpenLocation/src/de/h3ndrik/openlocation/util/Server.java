36558f0e81a1b3c60326fcca8526db7043098b8d
Hendrik Langer <dev@h3ndrik.de>
2012-11-28 23:13:41 +0100

    Server API stub

---
+++ OpenLocation/src/de/h3ndrik/openlocation/util/Server.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package de.h3ndrik.openlocation.util;
+
+/**
+ * @author h3ndrik
+ *
+ */
+public class Server {
+	
+	public void connect() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void disconnect() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public class API {
+		public void getLocation(String target, Long starttime, Long endtime) throws Exception {
+			throw new UnsupportedOperationException();
+			// get sender
+			// get token
+			// get target
+
+		}
+		public void setLocation() {
+			throw new UnsupportedOperationException();
+
+		}
+		public void deleteLocation() {
+			throw new UnsupportedOperationException();
+
+		}
+		public void getFriends() {
+			throw new UnsupportedOperationException();
+
+		}
+		public void requestFriend() {
+			throw new UnsupportedOperationException();
+			// This is sendrequestfriend!
+		}
+		public void removeFriend() {
+			throw new UnsupportedOperationException();
+
+		}
+		public void updatePreferences() {
+			throw new UnsupportedOperationException();
+			
+		}
+	}
+
+
+}
